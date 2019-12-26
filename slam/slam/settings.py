28044eb2b4805c18e1194b06dca9019548c5aeda
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2019-12-26 20:58:53 +0100

    First commit

--- slam/slam/settings.py
+++ slam/slam/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'slam',
+    'slam_core',
+    # 'slam_hardware',
+    # 'slam_network',
+    'slam_domain',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/login'
