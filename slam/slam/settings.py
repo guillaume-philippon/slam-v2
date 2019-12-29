9c19878e5166e02af7b11afd285e860f4300eb7a
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2019-12-29 18:51:16 +0100

    Some production preparation

--- slam/slam/settings.py
+++ slam/slam/settings.py
@@ -84,6 +84,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': os.path.join(BASE_DIR, 'conf/my.cnf'),
+    # }
 }
 
 
@@ -124,5 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT = '/var/www/html/static'
 
 LOGIN_URL = '/login'
