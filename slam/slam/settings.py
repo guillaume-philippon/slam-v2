b8179f74180b666acd46c0c5fe12107526f1ed15
root <root@slam.ijclab.in2p3.fr>
2023-01-10 10:38:38 +0100

    test change - to be reverted

--- slam/slam/settings.py
+++ slam/slam/settings.py
@@ -83,16 +83,16 @@ WSGI_APPLICATION = 'slam.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'OPTIONS': {
-    #         'read_default_file': os.path.join(BASE_DIR, 'my.cnf')
-    #     }
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'my.cnf')
+        }
+    }
 }
 
 
